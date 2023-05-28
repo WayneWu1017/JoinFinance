19fb903d8c7cc424fb4eb986c7dccc3cccaf9217
WayneWu1017 <mark1017777@gmail.com>
2023-05-28 16:40:51 +0800

    fix wrong position of module

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,3 +1,14 @@
 <template>
     <div>This is Home page.</div>
+    <CryptoPrice/>
 </template>
+
+<script>
+import CryptoPrice from '@/components/CryptoPrice.vue'
+export default {
+  name: 'App',
+  components: {
+    CryptoPrice,
+  }
+}
+</script>
